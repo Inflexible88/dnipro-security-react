db534bdb8af16aa26a01a865f9ddadb4ca9b4d0e
Inflexible88 <vinflexible@gmail.com>
2021-06-24 23:19:50 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <h1>Dnipro-security</h1>;
+}
---
+++ telegram.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const TelegramApi = require('node-telegram-bot-api');
+
+const PORT = process.env.PORT || 3002;
+
+const chatId = 479285632;
+const token = '1893426710:AAEfP3aWYYsSzrU6xLkQW7aeR4j7VflichU';
+
+const bot = new TelegramApi(token, { polling: true });
+
+app.use(express.json());
+app.post('/bot', (req, res) => {
+  res.send(req.body);
+  const { name = '', email = '', pnone = '', msg = '' } = req.body;
+  console.log(`Имя: ${req.body.name} Телефон: ${req.body.pnone}`);
+  try {
+    bot.sendMessage(
+      chatId,
+      `От: ${name}\n${pnone ? 'Телефон: ' + pnone : ''}
+      ${email !== '' ? '\nEmail: ' + email : ''} 
+      ${msg !== '' ? '\nВопрос: ' + msg : ''}`
+    );
+  } catch (e) {
+    console.log(`Что-то пошло не так. Телеграмм бот приказал долго жить...`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
