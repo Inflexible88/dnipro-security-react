9d3e0d4ebf336ea023246d700455e1a4b1544ae4
Inflexible88 <vinflexible@gmail.com>
2021-06-27 23:03:11 +0300

    FIx

--- telegram.js
+++ telegram.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const routes = require("./routes");
 const path = require('path');
 const TelegramApi = require('node-telegram-bot-api');
 
@@ -10,7 +9,6 @@ const token = '1893426710:AAEfP3aWYYsSzrU6xLkQW7aeR4j7VflichU';
 const bot = new TelegramApi(token, { polling: true });
 
 app.set("port", port);
-app.use('/', routes);
 app.use(express.json());
 app.use(express.static(path.join(__dirname+'/public')));
 
