3aa9468ebc8278519673e8d082bd1ad844349d6b
Inflexible88 <vinflexible@gmail.com>
2021-06-27 22:46:19 +0300

    Fix

--- telegram.js
+++ telegram.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const routes = require("./routes");
+const path = require('path');
 const TelegramApi = require('node-telegram-bot-api');
 
+const app = express();
 const PORT = process.env.PORT || 3002;
-
 const chatId = -1001582516206;
 const token = '1893426710:AAEfP3aWYYsSzrU6xLkQW7aeR4j7VflichU';
-
 const bot = new TelegramApi(token, { polling: true });
+
+app.set("port", port);
+app.use('/', routes);
+app.use(express.json());
+app.use(express.static(path.join(__dirname+'/public')));
+
 
 app.get('*', function (req, res) {
   const index = path.join(__dirname, 'build', 'index.html');
   res.sendFile(index);
 });
 
-app.use(express.json());
 app.post('/bot', (req, res) => {
   res.send(req.body);
   const { name = '', email = '', phone = '', msg = '' } = req.body;
