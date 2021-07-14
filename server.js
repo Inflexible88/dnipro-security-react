3499549422d7a62cdbe014857e48149b8e1f5ea2
Inflexible88 <vinflexible@gmail.com>
2021-07-14 15:24:37 +0300

    fix

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-const express = require('express');
-const path = require('path');
-var router = express.Router();
-const TelegramApi = require('node-telegram-bot-api');
+import express, { Router, json, static } from 'express';
+import { join } from 'path';
+import TelegramApi from 'node-telegram-bot-api';
 
 const app = express();
+const router = Router();
+
 const PORT = process.env.PORT || 3002;
 const chatId = -1001582516206;
 const token = '1893426710:AAEfP3aWYYsSzrU6xLkQW7aeR4j7VflichU';
 const bot = new TelegramApi(token, { polling: true });
 
 app.set('port', PORT);
-app.use(express.json());
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(json());
+app.use(static(join(__dirname + '/public')));
 
 // app.get('/', function (req, res) {
 //   const index = path.join(__dirname, 'build', 'index.html');
 //   res.sendFile(index);
 // });
+
+app.get("/", function(request, response){
+     
+  response.send("<h1>Главная страница</h1>");
+});
 
 app.post('/bot', (req, res) => {
   res.send(req.body);
