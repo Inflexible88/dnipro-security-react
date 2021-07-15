a1e40987d6dabb6c7e2f0b4428dd70becc9efa5a
Inflexible88 <vinflexible@gmail.com>
2021-07-15 10:40:43 +0300

    fix

--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-import express, { Router, json, static } from 'express';
-import { join } from 'path';
-import TelegramApi from 'node-telegram-bot-api';
+const express = require('express');
+const path = require('path');
+const TelegramApi = require('node-telegram-bot-api');
 
 const app = express();
-
 const PORT = process.env.PORT || 3002;
 const chatId = -1001582516206;
 const token = '1893426710:AAEfP3aWYYsSzrU6xLkQW7aeR4j7VflichU';
 const bot = new TelegramApi(token, { polling: true });
 
 // app.set('port', PORT);
-// app.use(json());
-// app.use(static(join(__dirname + '/public')));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname + '/public')));
 
-// app.get('/', function (req, res) {
-//   const index = path.join(__dirname, 'build', 'index.html');
-//   res.sendFile(index);
-// });
+app.use(express.static('build'))
+// app.use(express.static(path.resolve(__dirname, 'build')))
 
-app.get("/", function(request, response){
-     
-  response.send("<h1>Главная страница</h1>");
-});
 
 app.post('/bot', (req, res) => {
   res.send(req.body);
   const { topic = '', name = '', email = '', phone = '', msg = '' } = req.body;
+
   console.log(`Имя: ${req.body.name} Телефон: ${req.body.phone}`);
   try {
     bot.sendMessage(
@@ -37,6 +31,12 @@ app.post('/bot', (req, res) => {
   } catch (e) {
     console.log(`Что-то пошло не так. Телеграмм бот приказал долго жить...`);
   }
+});
+
+app.get('*',  (req, res) => {
+  // const index = path.join(__dirname, 'build', 'index.html');
+  const index = path.resolve(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 
